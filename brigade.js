04976a4e69d74c899f77d08849b00772e15a6980
Reinhard Nägele <unguiculus@gmail.com>
2018-04-27 15:43:19 +0200

    test

--- brigade.js
+++ brigade.js
@@ -1,12 +1,28 @@
-const { events, Job } = require("brigadier");
+const { events, Job, Group } = require("brigadier");
 
-events.on("push", function(e, project) {
-    console.log("Received push for commit " + e.commit);
-
-    var build = new Job("build", "docker:18.03.1-ce");
+events.on("push", function(event, project) {
+    var build = new Job("hello-brigade-build", "docker:18.03.1-ce");
+    build.privileged = true
+    build.storage.enabled = false;
     build.docker.enabled = true;
+    build.cache.enabled = false
     build.tasks = [
-        "docker build -t unguiculus/hello-brigade:1 ."
+        "docker build -t unguiculus/hello-brigade:1 src"
     ];
     build.run();
+
+    var push = new Job("hello-brigade-push", "docker:18.03.1-ce");
+    push.privileged = true
+    push.storage.enabled = false;
+    push.docker.enabled = true;
+    push.cache.enabled = false
+    push.env.DOCKER_USER = project.secrets.dockerUser
+    push.env.DOCKER_PASSWORD = project.secrets.dockerPassword
+    push.tasks = [
+        "docker login --username $DOCKER_USER --password $DOCKER_PASSWORD",
+        "docker push unguiculus/hello-brigade:1"
+    ];
+    push.run();
+
+    Group.runEach([build, push])
 });
