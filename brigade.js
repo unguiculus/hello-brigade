277299b325fd25f97c4bfa991c6a5ed85d9be145
Reinhard Nägele <unguiculus@gmail.com>
2018-04-27 12:03:01 +0200

    Add brigade.js

---
+++ brigade.js
@@ -0,0 +1,12 @@
+const { events, Job } = require("brigadier");
+
+events.on("push", function(e, project) {
+    console.log("Received push for commit " + e.commit);
+
+    var build = new Job("build", "docker:18.03.1-ce");
+    build.docker.enabled = true;
+    build.tasks = [
+        "docker build -t unguiculus/hello-brigade:1"
+    ];
+    build.run();
+});
